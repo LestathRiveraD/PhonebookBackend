9c534603c007873a7c056859f6afb14be3693501
LestathRiveraD <cesarrivera0404@gmail.com>
2025-08-23 21:43:18 -0700

    front

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 var morgan = require('morgan')
-var cors = require('cors')
+// var cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('content', req => {
   return JSON.stringify(req.body)
